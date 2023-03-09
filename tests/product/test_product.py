8e5b9c047fac3f8e051745d3a7d3e2e1c6e8e339
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-03-09 18:19:31 -0500

    requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        id = 1,
+        nome_da_empresa = "Matheus",
+        nome_do_produto = "resiliencia",
+        data_de_fabricacao = "25/07/1996",
+        data_de_validade = "25/07/2076",
+        numero_de_serie = "10",
+        instrucoes_de_armazenamento = "deixar ao ar livre"
+    )
+    assert produto.id == 1
+    assert produto.nome_da_empresa == "Matheus"
+    assert produto.nome_do_produto == "resiliencia"
+    assert produto.data_de_fabricacao == "25/07/1996"
+    assert produto.data_de_validade == "25/07/2076"
+    assert produto.numero_de_serie == "10"
+    assert produto.instrucoes_de_armazenamento == "deixar ao ar livre"
