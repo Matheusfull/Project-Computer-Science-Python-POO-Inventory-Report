8072e1e56d2d7c428eb25a499fb8bd80eb376631
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-03-10 12:41:57 -0500

    req 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,24 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(info):
+        fabricacao_velha = min([el["data_de_fabricacao"] for el in info])
+
+        # list_validade = [el["data_de_validade"] for el in list]
+        validade_perto = min([
+            el["data_de_validade"] for el in info
+            if el["data_de_validade"] > datetime.now().isoformat()
+        ])
+
+        moda_empresa = Counter(
+            el["nome_da_empresa"] for el in info
+            ).most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {fabricacao_velha}\n"
+            f"Data de validade mais próxima: {validade_perto}\n"
+            f"Empresa com mais produtos: {moda_empresa}"
+        )
