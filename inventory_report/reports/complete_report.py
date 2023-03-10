dbdca42e88407e0830640252254299d42cd3a132
Matheus Felipe <fpmitmatheus@yahoo.com>
2023-03-10 15:46:59 -0500

    requisito 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,18 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(self, list):
+        dados_simples = SimpleReport.generate(list)
+        lista_empresa = ''
+        emresas_objetos = Counter(el["nome_da_empresa"] for el in list)
+        for key, value in emresas_objetos.items():
+            lista_empresa += f"- {key}: {value}\n"
+
+        return (
+            f"{dados_simples}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{lista_empresa}"
+        )
